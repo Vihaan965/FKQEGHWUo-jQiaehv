6213952738bb68855d8f340b2e606be83440d79c
Vihaan965 <84268416+Vihaan965@users.noreply.github.com>
2021-12-27 17:44:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,59 @@
-var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg","https://i.postimg.cc/qqyYvVbq/grandpa.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
-var names = ["Fmaily Book","Ranbir Singh", "Diljeet Singh", "Rocky Singh", "Alia Singh", "Soni Singh"];
-var i = 0;
-function update()
+canvas = document.getElementById("myCanvas")
+ block_y=1;
+ block_x=1;
+block_image_width = 350;
+block_image_height = 430;
+
+var block_image_object= "";
+
+function new_image(get_image)
 {
-    i++;
-    var numbers_of_family_member_in_array = 5
-    if(i > numbers_of_family_member_in_array)
-      {
-          i = 0;
-      }
-    
-    //Debug the code to store list of images in updatedImage. Use images[i]
-    var updatedImage = [images];
-    //Debug the code to store list of names in updatedName. Use names[i]
-    var updatedName = [names] ;
- 
-    document.getElementById("family_member_image").src = updatedImage;
-    document.getElementById("family_member_name").innerHTML = updatedName;
+fabric.Image.fromURL( get_image , function (Img)  {
+	block_image_object  = Img;
+
+	block_image_object.scaleToWidth(block_image_width);
+	block_image_object.scaleToHeight(block_image_height);
+    block_image_object.set ({
+	top : block_y , 
+    left:  block_x
+	});
+canvas . add(block_image_object);
+});
 }
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+
+	if(keyPressed == '82') // add appropriate keycode
+	{
+		new_image('rr1.png');
+		console.log('r');
+		}
+	if(keyPressed == '71')
+	{
+		block_x = 200;
+		// upload green ranger
+	}
+	
+	if(keyPressed == '89')
+	{
+		block_x =350;
+		// upload yellow ranger
+	}
+	if(keyPressed == '80')
+	{
+		block_x = 600;
+		// upload pink ranger
+	}
+	if(keyPressed == '66')
+	{
+		block_x = 700;
+	// upload blue ranger
+	}
+	
+}
+
